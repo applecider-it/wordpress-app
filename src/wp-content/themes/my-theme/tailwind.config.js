c768faf37d2824544d3d8a6b2d9c89737111a2e6
owner <owner@localhost>
2026-07-02 13:57:37 +0900

    コメント

--- src/wp-content/themes/my-theme/tailwind.config.js
+++ src/wp-content/themes/my-theme/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // 独自プラグインのCSS管理もここでしている
   content: {
     files: ['./**/*.{php,js}', '../../plugins/my-plugin/templates/**/*.php'],
     exclude: ['../../plugins/my-plugin/templates/admin/**/*.php'],
